637d6008eafbb3a15a0b0931ea667aecbf6c263c
Daniel T!!! <danielt1836@gmail.com>
2025-10-12 22:57:40 -0400

    Update MODUELE3.cs

--- MODUELE3.cs
+++ MODUELE3.cs
@@ -1,1 +1,30 @@
+using System;
 
+public class Wine
+{
+  public decimal Price;
+  public int Year;
+  public Wine (decimal price)
+  {
+    Price = price;
+  }
+  public Wine (decimal price, int year) : this (price)
+  {
+    Year = year;
+  }
+  public void addYear() 
+{
+  Year++;
+}
+}
+
+public class Program 
+{
+  public static void Main()
+  {
+    Wine winny = new Wine(100000,2005);
+    winny.addYear();
+    Console.WriteLine($"Wine price: {winny.Price}, Year: {winny.Year}");
+    
+  }
+}
